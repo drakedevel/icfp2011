a88384939a7a1a2f5471fed54777767825b7647a
Matthew Ryan Maurer <maurer@ugcs.caltech.edu>
2011-06-17 23:25:26 -0700

    A few things in xbox fixed

--- xbox.hs
+++ xbox.hs
@@ -1,4 +1,5 @@
-data Move = Move AppDir Card Int
+import System.IO
+data Move = Move AppDir Card Int | Comment String
 
 data AppDir = L | R
 
@@ -7,15 +8,15 @@ data Card = Get | Succ | Double | Sc | Kc | Ic | Zero | Put
 data SKI = Ap SKI SKI | S | K | I
 
 apply :: Int -> Int -> Int -> [Move]
-apply dest l r = concat [num dest l, [Move L Get dest], inapp dest (Get : cardnum r)]
+apply dest l r = concat [num dest l, [Move L Get dest], inapp dest (Get : cardnum r), [Move R Zero dest]]
 applyInPlace :: Int -> Int -> [Move]
 applyInPlace dest r = inapp dest (Get : cardnum r)
 num :: Int -> Int -> [Move]
-num dest val = (Move R Put dest : (map (\x -> Move L x dest) $ cardnum val))
+num dest val = (Move L Put dest : Move R Zero dest : (map (\x -> Move L x dest) $ cardnum val))
 
 cardnum :: Int -> [Card]
 cardnum 0 = []
-cardnum n = if odd n then (Succ : Double : cardnum (n - 1)) else (Double : cardnum (n - 1))
+cardnum n = if odd n then (Succ : Double : cardnum ((n - 1) `div` 2)) else (Double : cardnum (n `div` 2))
 
 inapp :: Int -> [Card] -> [Move]
 inapp l = concatMap fuckapp
@@ -36,24 +37,31 @@ instance Show AppDir where
    show R = "2"
 
 instance Show Move where
-   show (Move dir card n) = unlines $ [show dir, show n, show card]
-
-comms :: [Move] -> String
-comms moves = concat $ map show moves
+   show (Move dir@L card n) = unlines $ [show dir, show card, show n]
+   show (Move dir@R card n) = unlines $ [show dir, show n, show card]
+   show (Comment str) = "Comment: " ++ str
+comms :: [Move] -> IO ()
+comms [] = return ()
+comms ((Comment str) : xs) = do hPutStrLn stderr str; comms xs
+comms (m@(Move _ _ _) : xs) = do putStr (show m); comms xs
 
 sizeofXBOX = 100
 
-xbox = concat $ (bomb 1 [2..]) : (apply 0 1 1) : [] -- (concat $ replicate sizeofXBOX $ applyInPlace 0 1) : [])
+xbox = bomb 1
+--xbox = concat $ (bomb 1) : (apply 0 1 1) : (concat $ replicate sizeofXBOX $ applyInPlace 0 1) : []
 
-bomb :: Int -> [Int] -> [Move]
-bomb target space = skiComp target space bombski
+bomb :: Int -> [Move]
+bomb target = skiComp target bombski
    where bombski :: SKI
-         bombski = I
-         --bombski = (Ap (Ap S (Ap (Ap S (Ap K S)) (Ap (Ap S (Ap K K)) I))) (Ap (Ap S (Ap (Ap S (Ap K S)) (Ap (Ap S (Ap K K)) I))) (Ap K I)))
+         --bombski = (Ap (Ap S (Ap K S)) (Ap (Ap S (Ap K K)) I))
+         bombski = (Ap (Ap S (Ap (Ap S (Ap K S)) (Ap (Ap S (Ap K K)) I))) (Ap (Ap S (Ap (Ap S (Ap K S)) (Ap (Ap S (Ap K K)) I))) (Ap K I)))
 
-skiComp :: Int -> [Int] -> SKI -> [Move]
-skiComp target (ia : ib : rest) (Ap a b) = concat [skiComp ia rest a, skiComp ib rest b, apply target ia ib]
-skiComp target _ v = [Move L Put target, Move R (skiToCard v) target]
+skiComp :: Int -> SKI -> [Move]
+skiComp target z = (Move L Put target) : (skiComp' z)
+   where skiComp' (Ap a b) = (skiComp' a) ++ (shift b)
+         skiComp'  v = [Move R (skiToCard v) target]
+         shift (Ap a b) = [Move L Kc target, Move L Sc target] ++ (shift a) ++ (shift b)
+         shift v = [Move R (skiToCard v) target]
 
 skiToCard S = Sc
 skiToCard K = Kc
@@ -78,4 +86,4 @@ commsLTG moves = unlines
          ltgCard Ic  = "CI"
          ltgCard Zero = "(CVal 0)"
 
-main = putStrLn $ comms xbox
+main = do hSetBuffering stderr LineBuffering; hSetBuffering stdout LineBuffering; hSetBuffering stdin LineBuffering; comms xbox; hFlush stdout; interact id
